cabd4679333c81af9814a18183d0b1bf3a552482
Baker Renneckar <bakerrenneckar@gmail.com>
2018-10-03 13:40:20 -0700

    Adjusted code to add both school type and student name together

--- check_connect.js
+++ check_connect.js
@@ -12,9 +12,9 @@ function fillArray(value, len) {
 
 var school_ids = {
   "Primary": "1AYzmnaBgflGrDkFSAlwRkfuUne0sQgwRx9rbIJOSzJQ",
-  "High": "1ZzfDPoHzMDYGIlhwzMDQ_gzNTnYY9DaAGT6aLYTyOC8"
-  // "Middle": "1uw0P8ain1GkMvj-NFTH3_11egvO4wTZS8OMqQj5J1dI",
-  // "Liberty": "1k6ULO-SCHU5D11gcyCWn6F_vjGz65pODQoRotokCiro"
+  "High": "1ZzfDPoHzMDYGIlhwzMDQ_gzNTnYY9DaAGT6aLYTyOC8",
+  "Middle": "1uw0P8ain1GkMvj-NFTH3_11egvO4wTZS8OMqQj5J1dI",
+  "Liberty": "1k6ULO-SCHU5D11gcyCWn6F_vjGz65pODQoRotokCiro"
 
 
 };
@@ -36,71 +36,98 @@ function select_report(){
 }
 
 
-function determine_start_row(report_sheet){
-  var connect_vals = report_sheet.getRange("C3:C").getValues();
-  var connect_len = connect_vals.filter(String).length;
+function determine_start_row(max_len){
 
-  var check_vals = report_sheet.getRange("B3:B").getValues();
-  var check_len = check_vals.filter(String).length;
+  var start_row = 3 + max_len;
 
-  var start_row = 3 + Math.max(check_len, connect_len)
-  Logger.log(check_len)
-  Logger.log(connect_len)
 
   return Number(start_row)
 
 }
 
-function check_and_connect(students, report_sheet, school_type, start_row) {
+function determine_max_len(report_sheet){
+  var connect_vals = report_sheet.getRange("C3:C").getValues();
+  var connect_len = connect_vals.filter(String).length;
 
-  for(i in students.slice(0,6)){
-    var check_range = students[i].getRange("C9:C14");
-    var student_name = students[i].getName();
+  var check_vals = report_sheet.getRange("B3:B").getValues();
+  var check_len = check_vals.filter(String).length;
+  var max_len = Math.max(check_len, connect_len)
 
-    if (check_range.isBlank()){
-      var check_vals = report_sheet.getRange("B3:B").getValues();
-      var check_len = check_vals.filter(String).length;
-      var check_range = report_sheet.getRange(check_len + start_row, 2)
-      check_range.setValues([[student_name]]);
-    }
 
-    var connect_range = students[i].getRange("C17:C26");
-
-    if (connect_range.isBlank()){
-      var connect_vals = report_sheet.getRange("C3:C").getValues();
-      var connect_len = connect_vals.filter(String).length;
-      var connect_range = report_sheet.getRange(connect_len + start_row, 3);
-      connect_range.setValues([[student_name]]);
-    }
-  }
-
-  var max_length = Math.max(check_len, connect_len) + 1
-  Logger.log(max_length)
-  var matrix_height = (max_length - start_row + 2)
-  var matrix = [];
-  for(var i=0; i<matrix_height + 1; i++) {
-    matrix[i] = new Array(school_type);
-  }
-
-  Logger.log(matrix)
-
-  var school_range = report_sheet.getRange(start_row,1, max_length, 1)
-
-  school_range.setValues(matrix);
-
+  return Number(max_len)
 
 }
 
 
-function main(){
-  for(var school_type in school_ids){
-    var students = select_school(school_ids[school_type]);
-    var report_sheet = select_report();
-    var start_row = determine_start_row(report_sheet)
-    Logger.log(Number(start_row))
 
-    check_and_connect(students, report_sheet, school_type, start_row);
+function check_and_connect(students, report_sheet, school_type) {
+  var missing_check = []
+  var missing_connect = []
+
+  for(i in students.slice(0,6)){
+
+    var check_range = students[i].getRange("C9:C14");
+    var student_name = students[i].getName();
+
+    if (check_range.isBlank()){
+      missing_check.push([school_type, student_name])
+
+    }
+
+
+    var connect_range = students[i].getRange("C17:C26");
+
+    if (connect_range.isBlank()){
+      missing_connect.push([school_type, student_name]);
+
+    }
+  }
+
+  var check_vals = report_sheet.getRange("B3:B").getValues();
+  var check_len = check_vals.filter(String).length;
+
+
+  var missing_check_range = report_sheet.getRange(3 + check_len, 1, missing_check.length, 2)
+  missing_check_range.setValues(missing_check)
+
+
+
+  var connect_vals = report_sheet.getRange("C3:C").getValues();
+  var connect_len = connect_vals.filter(String).length;
+
+
+  var missing_connect_range = report_sheet.getRange(3 + connect_len, 3, missing_connect.length, 2)
+  missing_connect_range.setValues(missing_connect)
+
+
+  return [missing_check, missing_connect]
+
+
 
   }
+
+
+
+
+function main(){
+  var complete_data = []
+  for(var school_type in school_ids){
+
+    var report_sheet = select_report();
+
+    var max_len = determine_max_len(report_sheet);
+    var start_row = determine_start_row(max_len);
+
+
+    var students = select_school(school_ids[school_type]);
+
+    var partial_data = check_and_connect(students, report_sheet, school_type);
+    complete_data.push(partial_data[0])
+    complete_data.push(partial_data[1])
+
+  }
+  Logger.log(complete_data[0])
+  Logger.log(complete_data[1])
+
 
 }
