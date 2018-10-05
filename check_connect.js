b2c5cbdbf35be7b7d7db6f46ba236a83be06b889
Baker Renneckar <bakerrenneckar@gmail.com>
2018-10-05 15:05:10 -0700

    broke code into multiple files

--- check_connect.js
+++ check_connect.js
@@ -1,69 +1,8 @@
-// Select Active Sheet and Define School Name Type
-  // ALL IDs are for Test Data
+// TO DO
 
-var school_ids = {
-  "Primary": "1AYzmnaBgflGrDkFSAlwRkfuUne0sQgwRx9rbIJOSzJQ",
-  "High": "1ZzfDPoHzMDYGIlhwzMDQ_gzNTnYY9DaAGT6aLYTyOC8",
-  "Middle": "1uw0P8ain1GkMvj-NFTH3_11egvO4wTZS8OMqQj5J1dI",
-  "Liberty": "1k6ULO-SCHU5D11gcyCWn6F_vjGz65pODQoRotokCiro"
-
-
-};
-
-function select_school(school_id){
-  var data_report = SpreadsheetApp.openById(school_id);
-  var students = data_report.getSheets();
-  return students
-
-}
-
-
-function select_report(){
-
-  var report = SpreadsheetApp.openById("13uDxiovTTiYLBwb06rmAwuMkiRyIn9AlU90f43qMCBE");
-  var report_sheet = report.getSheets()[0]
-  return report_sheet
-
-}
-
-
-
-function missing_data(students, school_type) {
-  var missing_check = []
-  var missing_connect = []
-
-  for(i in students.slice(0,6)){
-
-    var check_range = students[i].getRange("C9:C14");
-    var student_name = students[i].getName();
-
-    if (check_range.isBlank()){
-      missing_check.push([school_type, student_name])
-
-    }
-
-
-    var connect_range = students[i].getRange("C17:C26");
-
-    if (connect_range.isBlank()){
-      missing_connect.push([school_type, student_name]);
-
-    }
-  }
-
-  return [missing_check, missing_connect]
-
-  }
-
-
-function write_data(report_sheet, complete_data){
-    var missing_check_range = report_sheet.getRange(3, 1, complete_data[0].length, 2);
-    missing_check_range.setValues(complete_data[0]);
-
-  var missing_connect_range = report_sheet.getRange(3, 3, complete_data[1].length, 2);
-  missing_connect_range.setValues(complete_data[1]);
-
-}
+// Functions:
+// Determine start week - both for writing and reading
+// Based on start week, select quarter for writing.
 
 
 function main(){
