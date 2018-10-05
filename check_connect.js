ba911dc899912f34489576d61bcb0feba6575031
Baker Renneckar <bakerrenneckar@gmail.com>
2018-10-05 10:40:46 -0700

    Completed determine missing data function" git status

--- check_connect.js
+++ check_connect.js
@@ -1,12 +1,3 @@
-function fillArray(value, len) {
-  if (len == 0) return [];
-  var a = [value];
-  while (a.length * 2 <= len) a = a.concat(a);
-  if (a.length < len) a = a.concat(a.slice(0, len - a.length));
-  return a;
-}
-
-
 // Select Active Sheet and Define School Name Type
   // ALL IDs are for Test Data
 
@@ -36,31 +27,8 @@ function select_report(){
 }
 
 
-function determine_start_row(max_len){
 
-  var start_row = 3 + max_len;
-
-
-  return Number(start_row)
-
-}
-
-function determine_max_len(report_sheet){
-  var connect_vals = report_sheet.getRange("C3:C").getValues();
-  var connect_len = connect_vals.filter(String).length;
-
-  var check_vals = report_sheet.getRange("B3:B").getValues();
-  var check_len = check_vals.filter(String).length;
-  var max_len = Math.max(check_len, connect_len)
-
-
-  return Number(max_len)
-
-}
-
-
-
-function check_and_connect(students, report_sheet, school_type) {
+function missing_data(students, school_type) {
   var missing_check = []
   var missing_connect = []
 
@@ -83,51 +51,44 @@ function check_and_connect(students, report_sheet, school_type) {
     }
   }
 
-  var check_vals = report_sheet.getRange("B3:B").getValues();
-  var check_len = check_vals.filter(String).length;
-
-
-  var missing_check_range = report_sheet.getRange(3 + check_len, 1, missing_check.length, 2)
-  missing_check_range.setValues(missing_check)
-
-
-
-  var connect_vals = report_sheet.getRange("C3:C").getValues();
-  var connect_len = connect_vals.filter(String).length;
-
-
-  var missing_connect_range = report_sheet.getRange(3 + connect_len, 3, missing_connect.length, 2)
-  missing_connect_range.setValues(missing_connect)
-
-
   return [missing_check, missing_connect]
-
-
 
   }
 
 
+function write_data(report_sheet, complete_data){
+    var missing_check_range = report_sheet.getRange(3, 1, complete_data[0].length, 2);
+    missing_check_range.setValues(complete_data[0]);
+
+  var missing_connect_range = report_sheet.getRange(3, 3, complete_data[1].length, 2);
+  missing_connect_range.setValues(complete_data[1]);
+
+}
 
 
 function main(){
-  var complete_data = []
+  var missing_check = [];
+  var missing_connect = [];
+  var complete_data = [[],[]];
+
+  var report_sheet = select_report();
   for(var school_type in school_ids){
-
-    var report_sheet = select_report();
-
-    var max_len = determine_max_len(report_sheet);
-    var start_row = determine_start_row(max_len);
 
 
     var students = select_school(school_ids[school_type]);
 
-    var partial_data = check_and_connect(students, report_sheet, school_type);
-    complete_data.push(partial_data[0])
-    complete_data.push(partial_data[1])
+    data = missing_data(students, school_type);
 
+    var partial_check = data[0];
+    var partial_connect = data[1];
+    for(i in partial_check){
+      complete_data[0].push(partial_check[i]);
+
+    }
+
+    for(i in partial_connect){
+      complete_data[1].push(partial_connect[i]);
+    }
   }
-  Logger.log(complete_data[0])
-  Logger.log(complete_data[1])
-
-
+  write_data(report_sheet, complete_data);
 }
