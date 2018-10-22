c31de12566632d74970cb7ef75ab940b0a3ccc3d
Baker Renneckar <bakerrenneckar@gmail.com>
2018-10-21 20:54:35 -0700

    corrected columns for connect writing

--- variables.js
+++ variables.js
@@ -20,8 +20,8 @@ var report_q1_check_columns = {
 
 var report_q1_connect_columns = {
   "Week 1": 3, "Week 2": 7, "Week 3": 11, "Week 4": 15,
-  "Week 5": 19, "Week 6": 23, "Week 7": 27, "Week 8": 32,
-  "Week 9": 36
+  "Week 5": 19, "Week 6": 23, "Week 7": 27, "Week 8": 31,
+  "Week 9": 35
 }
 
 
@@ -33,8 +33,8 @@ var report_q2_check_columns = {
 
 var report_q2_connect_columns = {
   "Week 10": 3, "Week 11": 7, "Week 12": 11, "Week 13": 15,
-  "Week 14": 19, "Week 15": 23, "Week 16": 27, "Week 17": 32,
-  "Week 18": 36
+  "Week 14": 19, "Week 15": 23, "Week 16": 27, "Week 17": 31,
+  "Week 18": 35
 }
 
 var report_q3_check_columns = {
@@ -45,8 +45,8 @@ var report_q3_check_columns = {
 
 var report_q3_connect_columns = {
   "Week 19": 3, "Week 20": 7, "Week 21": 11, "Week 22": 15,
-  "Week 23": 19, "Week 24": 23, "Week 25": 27, "Week 26": 32,
-  "Week 27": 36
+  "Week 23": 19, "Week 24": 23, "Week 25": 27, "Week 26": 31,
+  "Week 27": 35
 }
 
 var report_q4_check_columns = {
@@ -57,8 +57,8 @@ var report_q4_check_columns = {
 
 var report_q4_connect_columns = {
   "Week 28": 3, "Week 29": 7, "Week 30": 11, "Week 31": 15,
-  "Week 32": 19, "Week 33": 23, "Week 34": 27, "Week 35": 32,
-  "Week 36": 36
+  "Week 32": 19, "Week 33": 23, "Week 34": 27, "Week 35": 31,
+  "Week 36": 35
 }
 
 
